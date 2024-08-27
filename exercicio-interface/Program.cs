fd222f13de1beb9e8f7a889215b9b48339680598
joaovsa97 <joaos.azevedo10@gmail.com>
2024-08-27 14:20:29 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio-interface/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using exercicio_interface.Entities;
+using exercicio_interface.Services;
+
+namespace exercicio_interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os dados da locação");
+            Console.Write("Modelo do veiculo: ");
+            string model = Console.ReadLine();
+            Console.Write("Data da locação do veículo(dd/MM/aaaa HH:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Data da devolução do veículo(dd/MM/aaaa HH:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Digite o valor a pagar por hora: ");
+            double priceHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite o valor a pagar por dia: ");
+            double priceDay = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            RentalService rentalService = new RentalService(priceHour, priceDay, new BrasilTaxService());
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("INVOICE:");
+            Console.WriteLine(carRental.Invoice);
+        }
+    }
+}
---
+++ exercicio-interface/Services/RentalService.cs
@@ -0,0 +1,38 @@
+﻿using exercicio_interface.Entities;
+
+namespace exercicio_interface.Services
+{
+    internal class RentalService
+    {
+        public double PriceHour { get; private set; }
+        public double PriceDay { get; private set; }
+
+        private ITaxService _TaxService;
+
+        public RentalService(double priceHour, double priceDay, ITaxService taxService)
+        {
+            PriceHour = priceHour;
+            PriceDay = priceDay; 
+            _TaxService = taxService;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+
+            double basicPayment = 0.0;
+
+            if(duration.TotalHours <= 12.0)
+            {
+                basicPayment = PriceHour * Math.Ceiling(duration.TotalHours);
+            } else
+            {
+                basicPayment = PriceDay * Math.Ceiling(duration.TotalDays);
+            }
+
+            double tax = _TaxService.Tax(basicPayment);
+
+            carRental.Invoice = new Invoice(basicPayment, tax);
+        }
+    }
+}
